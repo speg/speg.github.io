dfc47a439fb7b0829f77524858e0664301161168
steve hiemstra <stevehiemstra@gmail.com>
2014-03-22 12:51:01 -0400

    github commits

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var request = require('request');
 var express = require('express');
 var app = express();
 var twitter = require('./lib/twitter.js');
@@ -7,7 +8,7 @@ app.set('view engine', 'jade');
 app.all('*', function(req, res, next) {
     if (req.headers.origin && req.headers.origin.indexOf('speg.com') > -1){
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Content-Type', 'application/json; charset=utf-8');
     }
@@ -23,5 +24,42 @@ app.get('/tweets/', function(req, res){
     var tweets = twitter.getUserTimeline({count: 10}, err, suc);
 });
 
+app.get('/commits/', function (req, res){
+    returnCommits(res);
+});
+
+
+function returnCommits(res){
+    var GITHUB = process.env.GITHUB,
+    GPASS = process.env.GPASS;
+
+    request({
+        url: GITHUB,
+        auth: {user: 'speg', pass: GPASS},
+        headers: {'User-Agent': 'npm requests'}
+    }, function(error, response, body){
+        var parsed = parseCommits(body);
+        res.send(parsed);
+    }); 
+};
+
 app.listen(3000);
 
+function parseCommits(body){
+    //parse incoming list of activities
+    var obj = JSON.parse(body).slice(0, 20);
+    var result = [];
+    obj.forEach(function(o){
+        console.log(o);
+        if (o.type === 'PushEvent' && o.payload.commits){
+            o.payload.commits.forEach(function(c){
+                result.push({
+                    type: 'commit',
+                    title: c.message,
+                    url: c.url
+                });
+            });
+        }
+    });
+    return result.slice(0, 10);
+}
--- javascript/main.js
+++ javascript/main.js
@@ -10,4 +10,15 @@ $(function(){
         $.each(data, placement);
         $('div.tweets>ul').append(html);
     }});
+
+    $.ajax('//api.speg.com/commits/', {success: function(data){
+        //console.log(data);    
+        var placement, html;
+        html = '';
+        placement = function(index, value) {
+            html = '<a href="' + value.url + '"><li>' + value.title + '</li></a>' + html;
+        };
+        $.each(data, placement);
+        $('div.commits>ul').append(html);
+    }});
 });
